d056c5f253ca3ba0d5256a804abe0f84134d0be8
Viktoriia Plotnikova <viktoriia287@gmail.com>
2021-02-10 00:29:31 +0300

    tests were created

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,75 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int maxRadioWave = 9;
+    private int minRadioWave;
+    private int currentRadioWave;
 
+    private int maxSoundLevel = 10;
+    private int minSoundLevel;
+    private int currentSoundLevel;
+
+// Radio waves
+
+    public int getMaxRadioWave() {
+        return maxRadioWave;
+    }
+
+    public void setMaxRadioWave(int maxRadioWave) {
+        this.maxRadioWave = maxRadioWave;
+    }
+
+    public int getMinRadioWave() {
+        return minRadioWave;
+    }
+
+    public void setMinRadioWave(int minRadioWave) {
+        this.minRadioWave = minRadioWave;
+    }
+
+    public int getCurrentRadioWave() {
+        return currentRadioWave;
+    }
+
+    public void setCurrentRadioWave(int currentRadioWave) {
+        if (currentRadioWave > maxRadioWave) {
+            currentRadioWave = minRadioWave;
+        }
+        if (currentRadioWave < minRadioWave) {
+            currentRadioWave = maxRadioWave;
+        }
+        this.currentRadioWave = currentRadioWave;
+    }
+
+// Radio sound levels
+
+    public int getMaxSoundLevel() {
+        return maxSoundLevel;
+    }
+
+    public void setMaxSoundLevel(int maxSoundLevel) {
+        this.maxSoundLevel = maxSoundLevel;
+    }
+
+    public int getMinSoundLevel() {
+        return minSoundLevel;
+    }
+
+    public void setMinSoundLevel(int minSoundLevel) {
+        this.minSoundLevel = minSoundLevel;
+    }
+
+    public int getCurrentSoundLevel() {
+        return currentSoundLevel;
+    }
+
+    public void setCurrentSoundLevel(int currentSoundLevel) {
+        if (currentSoundLevel > maxSoundLevel) {
+            currentSoundLevel = maxSoundLevel;
+        }
+        if (currentSoundLevel < minSoundLevel) {
+            currentSoundLevel = minSoundLevel;
+        }
+        this.currentSoundLevel = currentSoundLevel;
+    }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,39 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @Test
+    public void validateRadioWaveOverPossible() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioWave());
+        radio.setCurrentRadioWave(10);
+        assertEquals(0, radio.getCurrentRadioWave());
+    }
+
+    @Test
+    public void validateRadioWaveLessPossible() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioWave());
+        radio.setCurrentRadioWave(-1);
+        assertEquals(9, radio.getCurrentRadioWave());
+    }
+
+    @Test
+    public void validateChangeSoundLevelOverPossible() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentSoundLevel());
+        radio.setCurrentSoundLevel(15);
+        assertEquals(10, radio.getCurrentSoundLevel());
+    }
+
+    @Test
+    public void validateChangeSoundLevelUnderPossible() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentSoundLevel());
+        radio.setCurrentSoundLevel(-8);
+        assertEquals(0, radio.getCurrentSoundLevel());
+    }
 }
